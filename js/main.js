1ef62d84a8a72ccb607473c358c5b771d93aa417
sivasivasivadotcom <105623481+sivasivasivadotcom@users.noreply.github.com>
2022-11-04 16:55:00 +0900

    Add: 機能追加
    結果画面で１分あたりの文字数を計算できるようにしました。

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@
 	const retryBtn = document.getElementById('retryBtn');
 	const timeCounter = document.getElementById('timeCounter');
 	const resultLength = document.getElementById('resultLength');
+	const resultMinute = document.getElementById('resultMinute');
+	const resultParMinute = document.getElementById('resultParMinute');
 	const resultArea = document.getElementById('resultArea');
 	const exampleArea = document.getElementById('exampleArea');
 	const exampleBtn = document.getElementById('exampleBtn');
@@ -77,11 +79,13 @@
 		resultArea.classList.remove('hidden');
 		typeareaCount = typearea.value.replace(/\n| |　/g, "");
 		resultLength.textContent = typeareaCount.length;
+		resultMinute.textContent = timeM;
+		resultParMinute.textContent = Math.round(60 * typeareaCount.length / time);
 	}
 
 	// timeを1sごとに増やす
 	function timeCount() {
-		if (time === 600) {// 後で600に戻す
+		if (time === 600) {
 			textCount();
 			return;
 		}
